1e934403a86e580c0331ba6c2d800c16da023728
severjanin <severjanin@gmail.com>
2018-09-10 18:45:29 +0300

    commit message

---
+++ IdeaProjects/SomeClass/src/ee/sda/Main.java
@@ -0,0 +1,8 @@
+package ee.sda;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ IdeaProjects/SomeClass/src/ee/sda/SomeClass.java
@@ -0,0 +1,12 @@
+package ee.sda;
+
+public class SomeClass {
+
+    private int someInt;
+    private String someStr;
+
+    public SomeClass(int someInt, String someStr) {
+        this.someInt = someInt;
+        this.someStr = someStr;
+    }
+}
